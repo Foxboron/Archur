f361cee06a5c2cc9901190b3f3fd97c6c93fb4e6
Morten Linderud <morten@linderud.pw>
2016-04-26 01:28:19 +0200

    Temp license
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import find_packages, setup
 setup(
     name="Archur",
     version="0.0.1",
-    author="Morten Linderud, Gregor Best",
-    author_email="morten@linderud.pw, gbe@unobtanium.de",
-    description="nREPL implementation in Hylang",
-    long_description="nREPL implementation in Hylang",
+    author="Morten Linderud",
+    author_email="morten@linderud.pw",
+    description="Arch linux wallpaper generator",
+    long_description="Arch linux wallpaper generator",
     license="MIT",
     scripts=["archur.py"],
     entry_points={
@@ -21,7 +21,7 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Lisp",
+        "Programming Language :: Python",
         "Topic :: Software Development :: Libraries",
     ]
 )
