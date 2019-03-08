d2a2d2f15989a429d7112224597a3a41d89f20b6
Deryugin <deryugin.denis@gmail.com>
2019-03-08 18:10:20 +0300

    Add options to scale text and logo
    This can be useful for generating vertical images (i.e. 1080x1920),
    with default size logo may look too small

--- archur.py
+++ archur.py
@@ -38,7 +38,7 @@ def text_draw(draw, text, pixel, ress, theme):
         base_height += (H/100)*8
 
 
-def generate_img(output="", theme={}, text="", resolution=(1920,1080)):
+def generate_img(output="", theme={}, text="", resolution=(1920,1080), text_scale=1.0, logo_scale=1.0):
 
     # img = Image.open(backdrop)
     img = Image.new("RGB", resolution, theme["background"])
@@ -46,13 +46,13 @@ def generate_img(output="", theme={}, text="", resolution=(1920,1080)):
 
     logo = Image.open(DEFAULT_DIR+"/assets/logo.png")
     colorized_img = ImageOps.colorize(logo.convert("L"), theme["text"], theme["background"])
-    size = int((W/100)*17)
+    size = int((W/100)*17*logo_scale)
     logo_newsize = colorized_img.resize((size, size), Image.ANTIALIAS)
     img.paste(logo_newsize, (int((W-size)/2), int((H-size)/2)))
 
     draw = ImageDraw.Draw(img)
 
-    base_font_pixle = int((56/1920)*resolution[0])
+    base_font_pixle = int((56/1920)*resolution[0]*text_scale)
 
     font = ImageFont.truetype("DejaVuSansMono.ttf", base_font_pixle)
     w, h = font.getsize(text)
@@ -68,6 +68,8 @@ def main():
     parser.add_argument('-t','--theme', default=get_random_theme(), help='The theme to use, else random. \'black\' or \'solarized\'', required=False)
     parser.add_argument('--text', default=get_random_text(), help='Text on the picture, or random', required=False)
     parser.add_argument('-r', '--resolution', default=(1920,1080), help='Sets the resolution of the image. Example: 1920x1080', required=False)
+    parser.add_argument('-ts', '--text-scale', default=(1.0), help='Sets scale for the text. Example: 1.75', required=False)
+    parser.add_argument('-ls', '--logo-scale', default=(1.0), help='Sets scale for the logo. Example: 3.0', required=False)
     parser.add_argument('-fg', '--foreground-color', type=str, help='Color for the text and the logo.', required=False)
     parser.add_argument('-bg', '--background-color', type=str, help='Color for the background.', required=False)
     args = vars(parser.parse_args())
@@ -92,7 +94,8 @@ def main():
         except: pass
 
 
-    generate_img(output=output, theme=args["theme"], text=args["text"], resolution=args["resolution"])
+    generate_img(output=output, theme=args["theme"], text=args["text"], resolution=args["resolution"],
+        text_scale=float(args["text_scale"]), logo_scale=float(args["logo_scale"]))
 
 
 if __name__ == '__main__':
